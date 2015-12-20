e4e6d7922c148fdf2edcf1f329b4314f7a620cd4
femnad <zehemer@fastmail.fm>
2015-12-20 16:20:48 +0200

    Add Fedora Packaging Tasks

---
+++ init-pia-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://www.privateinternetaccess.com/openvpn/openvpn.zip -O /tmp/openvpn.zip
+ovpn_scripts_dir="~/z/ce"
+unzip /tmp/openvpn.zip $ovpn_scripts_dir
+auth_user_pass_file="$ovpn_scripts_dir/up"
+pass www/private-internet-access | tail -n 1 | awk -F ': ' '{print $1}' > "$auth_user_pass_file"
+pass www/private-internet-access >> "$auth_user_pass_file"
+for ovpn_file in "$ovpn_scripts_dir/*.ovpn"
+do
+    echo 'auth-user-pass up' >> $ovpn_file
+done
+chmod 600 "$auth_user_pass_file"
