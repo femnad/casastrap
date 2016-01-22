353f9d7b7f9176d79bdb91004715837a79e31817
femnad <zehemer@fastmail.fm>
2016-01-22 19:40:03 +0200

    Root Install Mopidy

--- install-mopidy.sh
+++ install-mopidy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
+libspotify_extract_location="$1"
 timestamp=$(date +"%s")
 mkdir "/tmp/$timestamp"
 temp_archive="/tmp/$timestamp/libspotify.tar.gz"
 wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz -O $temp_archive
-tar xvf $temp_archive -C ~/z/dy/
-sudo su -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local-lib.conf'
-cd ~/z/dy/libspotify-12.1.51-Linux-x86_64-release
-sudo make install
-pip install mopidy --user
-pip install pyspotify --user
-pip install Mopidy-Spotify --user
-pip install Mopidy-Spotify-Tunigo --user
+tar xvf $temp_archive -C $libspotify_extract_location
+echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local-lib.conf
+cd $libspotify_extract_location/libspotify-12.1.51-Linux-x86_64-release
+make install
+pip install mopidy
+pip install pyspotify
+pip install Mopidy-Spotify
+pip install Mopidy-Spotify-Tunigo
 cat << EOF >> "$HOME/.profile"
-$PATH="$PATH:$HOME:/.local/bin"
+PATH="\$PATH:\$HOME:/.local/bin"
 EOF
