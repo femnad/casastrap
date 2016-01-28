c6ef3ed8a1e9452abd5a4bcd83d2fbeda4fc1946
femnad <zehemer@fastmail.fm>
2016-01-28 15:08:12 +0200

    Variablize Some File and Directory Names

--- init-pass.sh
+++ init-pass.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gpg --import ~/fff.key
-pass init femnad@fastmail.fm
+key_file_location="$1"
+gpg_id="$2"
+gpg --import $key_file_location
+pass init $gpg_id
--- symlink-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p ~/bin
-cd ~/bin
-find ~/z/fm/{protection-from-scriptification,dzenitor,ysnpassmenu}/* -executable -type f -exec ln -s '{}' \;
