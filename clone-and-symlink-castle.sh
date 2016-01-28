188403ba58bc125fdad03b3dd316c645dcc1ab6c
femnad <zehemer@fastmail.fm>
2016-01-28 14:41:25 +0200

    Improve Homesick Cloning Script

--- clone-and-symlink-castle.sh
+++ clone-and-symlink-castle.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -e
 source ~/.profile
 homesick clone git@gitlab.com:femnad/base.git
-homesick link base
+homesick link -s base
 homesick clone git@gitlab.com:femnad/homebin.git
-homesick link homebin
+homesick link -s homebin
 hostname=$(hostname)
-homesick clone git@gitlab.com:femnad/$hostname.git
-homesick link $hostname
+# Is this a regular host (Hope you keep naming hosts after elements)
+curl -s http://periodic.lanl.gov/list.shtml | lynx -stdin -dump -nolist | sed -n '/Elements with their Symbol/,/Resources/p' | grep -i $hostname
+if [ "$?" -eq "0" ]; then
+    castle=$hostname
+else
+    castle=basic
+fi
+homesick clone git@gitlab.com:femnad/$castle.git
+homesick link -s $castle
