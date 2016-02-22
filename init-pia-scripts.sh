0ece974c7ee92bdc9b233cea43cfbcad5ade219e
femnad <zehemer@fastmail.fm>
2016-02-22 15:00:25 +0200

    Fix PIA Script Initialization

--- init-pia-scripts.sh
+++ init-pia-scripts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+scripts_dir="$1"
 wget https://www.privateinternetaccess.com/openvpn/openvpn.zip -O /tmp/openvpn.zip
-ovpn_scripts_dir=~/z/ce
-mkdir -p $ovpn_scripts_dir
-unzip /tmp/openvpn.zip -d $ovpn_scripts_dir
-auth_user_pass_file="$ovpn_scripts_dir/up"
-pass www/private-internet-access | tail -n 1 | awk -F ': ' '{print $1}' > "$auth_user_pass_file"
+mkdir -p $scripts_dir
+unzip -u /tmp/openvpn.zip -d $scripts_dir
+auth_user_pass_file="$scripts_dir/up"
+pass www/private-internet-access | tail -n 1 | awk -F ': ' '{print $2}' > "$auth_user_pass_file"
 pass www/private-internet-access | head -n 1 >> "$auth_user_pass_file"
-for ovpn_file in "$ovpn_scripts_dir/*.ovpn"
+for ovpn_file in "$scripts_dir/"*.ovpn
 do
+    sed -i '/^$/d' "$ovpn_file"
     echo 'auth-user-pass up' >> "$ovpn_file"
 done
 chmod 600 "$auth_user_pass_file"
