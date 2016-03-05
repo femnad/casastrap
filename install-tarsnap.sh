b587940acb8da1391c66e55117e84b96f6024489
femnad <zehemer@fastmail.fm>
2016-03-05 14:24:39 +0200

    Improve Tarsnap Installation

--- install-tarsnap.sh
+++ install-tarsnap.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
+
+install_dir="$1"
+
 temp_dir="/tmp/$(date +"%s")"
 mkdir $temp_dir
 wget https://www.tarsnap.com/download/tarsnap-autoconf-1.0.36.1.tgz -O /tmp/tarsnap-autoconf-1.0.36.1.tgz
-install_base="/home/femnad/z/dy"
-mkdir -p $install_base
-install_dir="$install_base/tarsnap-autoconf-1.0.36.1"
-tar xvf /tmp/tarsnap-autoconf-1.0.36.1.tgz -C $install_base
-cd $install_dir
+mkdir -p $install_dir
+extract_dir="$install_dir/tarsnap-autoconf-1.0.36.1"
+tar xvf /tmp/tarsnap-autoconf-1.0.36.1.tgz -C $install_dir
+cd $extract_dir
 ./configure
 make all
-sudo make install
+make install
