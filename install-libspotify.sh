4ca00bd0e1ae6e6bff68ed2c0a3d5f9dc7864fe7
femnad <zehemer@fastmail.fm>
2016-03-05 12:32:17 +0200

    Introduce Some Sanity, Starting with Mopidy
    * Use virtualenv for install pip packages.
    * Installing libspotify is rightfully a separate task.
    * Use a variables file.

--- install-libspotify.sh
+++ install-libspotify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+
 libspotify_extract_location="$1"
+
 timestamp=$(date +"%s")
 mkdir "/tmp/$timestamp"
 temp_archive="/tmp/$timestamp/libspotify.tar.gz"
@@ -9,10 +11,3 @@ tar xvf $temp_archive -C $libspotify_extract_location
 echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local-lib.conf
 cd $libspotify_extract_location/libspotify-12.1.51-Linux-x86_64-release
 make install
-pip install mopidy
-pip install pyspotify
-pip install Mopidy-Spotify
-pip install Mopidy-Spotify-Tunigo
-cat << EOF >> "$HOME/.profile"
-PATH="\$PATH:\$HOME:/.local/bin"
-EOF
