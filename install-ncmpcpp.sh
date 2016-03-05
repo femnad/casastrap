945f62bdf4fcb57559fac334f5f5ea40bf058d35
femnad <zehemer@fastmail.fm>
2016-03-05 13:26:31 +0200

    Improvements for Ncmpcpp Install
    * Also add portage for homesick install playbook

--- install-ncmpcpp.sh
+++ install-ncmpcpp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e
+
+install_dir="$1"
+
 temp_dir="/tmp/$(date +"%s")"
+mkdir $temp_dir
 cd $temp_dir
 wget http://rybczak.net/ncmpcpp/stable/ncmpcpp-0.7.tar.bz2
-mkdir -p ~/z/dy/
-tar xvf ncmpcpp-0.7.tar.bz2 -C ~/z/dy/
-cd ~/z/dy/ncmpcpp-0.7/
+mkdir -p "$install_dir"
+tar xvf ncmpcpp-0.7.tar.bz2 -C "$install_dir"
+cd "$install_dir/ncmpcpp-0.7"
 autoreconf --install --force
 ./configure
 make
