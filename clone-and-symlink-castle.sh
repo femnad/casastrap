426cf492ccb31eb8d19f05aa323194b8ad3f9b78
femnad <zehemer@fastmail.fm>
2016-03-05 13:49:45 +0200

    Homesick Cloning Playbook Additions

--- clone-and-symlink-castle.sh
+++ clone-and-symlink-castle.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 source ~/.profile
 homesick clone git@gitlab.com:femnad/base.git
 homesick link -s base
 homesick clone git@gitlab.com:femnad/homebin.git
-homesick link -s homebin
+#homesick link -s homebin
 hostname=$(hostname)
 # Is this a regular host (Hope you keep naming hosts after elements)
 curl -s http://periodic.lanl.gov/list.shtml | lynx -stdin -dump -nolist | sed -n '/Elements with their Symbol/,/Resources/p' | grep -i $hostname
