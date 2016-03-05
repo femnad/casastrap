32c64ac6bc34b90fc801d5712f053e2466e6b9ec
femnad <zehemer@fastmail.fm>
2016-03-05 14:59:39 +0200

    Sanitify Firefox Initializer Playbook

--- init-firefox.sh
+++ init-firefox.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd ~/z/gl/dactyl/pentadactyl
+set -e
+
+repo_path="$1"
+
+cd $repo_path/pentadactyl
 DISPLAY=:0 make installxpi
-DISPLAY=:0 firefox https://addons.mozilla.org/firefox/downloads/latest/6366/addon-6366-latest.xpi
