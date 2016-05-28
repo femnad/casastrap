9cfe29bf04a0a0a496c0dca72a21241e0ab0f251
femnad <zehemer@fastmail.fm>
2016-05-28 16:47:51 +0300

    Use Homeshick for Managing Dotfiles

--- add-gems-bin-to-path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ruby_version=$(ls $HOME/.gem/ruby/)
-cat << EOF >> ~/.profile
-PATH="\$PATH:\$HOME/.gem/ruby/$ruby_version/bin"
-EOF
--- clone-and-symlink-castle.sh
+++ clone-and-symlink-castle.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-source ~/.profile
-homesick clone git@gitlab.com:femnad/base.git
-homesick link -s base
-homesick clone git@gitlab.com:femnad/homebin.git
-#homesick link -s homebin
+source $HOME/.homesick/repos/homeshick/homeshick.sh 
+homeshick clone git@gitlab.com:femnad/base.git
+homeshick link -s base
+homeshick clone git@gitlab.com:femnad/homebin.git
+homeshick link -s homebin
 hostname=$(hostname)
-# Is this a regular host (Hope you keep naming hosts after elements)
-curl -s http://periodic.lanl.gov/list.shtml | lynx -stdin -dump -nolist | sed -n '/Elements with their Symbol/,/Resources/p' | grep -i $hostname
+# Is this a host with a dedicated castle
+# (Hope you keep naming hosts after elements)
+lynx -dump -nolist http://www.ptable.com/ | egrep -o '[0-9]{1,3} [A-Za-z]{1,3} [A-Za-z]+' | grep -i $hostname
 if [ "$?" -eq "0" ]; then
     castle=$hostname
 else
     castle=basic
 fi
-homesick clone git@gitlab.com:femnad/$castle.git
-homesick link -s $castle
+homeshick clone git@gitlab.com:femnad/$castle.git
+homeshick link -s $castle
